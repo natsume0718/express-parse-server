64fb4ac2bc11b7352b4ddba289b0ff2475181c2f
natsume0718 <moto718program@gmail.com>
2021-01-02 17:03:31 +0900

    課題

---
+++ README.md
@@ -0,0 +1,3 @@
+課題 3
+
+https://airtable.com/tblTnXBXFOYJ0J7lZ/viwyi8muFtWUlhNKG/reck0YmhBW8SQ0tBn
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+  res.send(200, { text: 'hello world' });
+});
+router.post('/', function (req, res, next) {
+  if (!req.is('application/json')) {
+    res.send(400);
+  }
+  res.send(201, req.body);
 });
 
 module.exports = router;
